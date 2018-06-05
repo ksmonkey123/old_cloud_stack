180083294c93d1cdb532e81d6dc39a15a43f5f2d
Andreas Wälchli <andreas.waelchli@me.com>
2018-06-05 10:49:27 +0200

    added automated client install

--- update.sh
+++ update.sh
@@ -4,3 +4,10 @@ mvn clean install -DskipTests
 docker-compose build
 docker-compose create
 docker-compose restart
+
+echo installing client...
+
+rm -rvf /var/www/html/*
+cp -rv client/* /var/www/html/
+
+echo done
