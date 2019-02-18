b39f7c7414be7eb1a3f33cc8973b365c8a13a37c
Andreas <andreas.waelchli@me.com>
2019-02-18 22:52:33 +0100

    added norestart update script

---
+++ update-norestart.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mvn clean install
+docker-compose build
+docker-compose create
+#docker-compose restart
+
+echo installing client...
+
+rm -rvf /var/www/html/*
+cp -rv client/dist/cloud-client/* /var/www/html/
+
+echo done
