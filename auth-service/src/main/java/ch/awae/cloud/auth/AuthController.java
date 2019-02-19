6d53f796c9c0873d99d19751f42a709165ba91f0
Andreas Wälchli <andreas.waelchli@me.com>
2019-02-19 18:13:45 +0100

    authService: separated auth and admin tasks into separate RestControllers

--- auth-service/src/main/java/ch/awae/cloud/auth/AuthController.java
+++ auth-service/src/main/java/ch/awae/cloud/auth/AuthController.java
@@ -1,9 +1,5 @@
 package ch.awae.cloud.auth;
 
-import java.util.Arrays;
-import java.util.List;
-
-import javax.transaction.Transactional;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -11,12 +7,9 @@ import javax.validation.constraints.Size;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ch.awae.cloud.exception.BadRequestException;
@@ -24,14 +17,12 @@ import ch.awae.cloud.exception.ResourceNotFoundException;
 import ch.awae.cloud.security.Security;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.val;
 
 @AllArgsConstructor
 @RestController
 public class AuthController {
 
 	private UserRepo repo;
-	private RoleRepository roleRepo;
 	private RoleService roleService;
 	private PasswordEncoder crypto;
 	private TokenService token;
@@ -72,68 +63,6 @@ public class AuthController {
 		return repo.findById(userId).orElseThrow(() -> new ResourceNotFoundException("user", "id", userId));
 	}
 
-	@Secured("ROLE_ADMIN")
-	@PostMapping("/user/{userId}/password")
-	public void changeOtherPasssword(@PathVariable("userId") Long userId,
-			@Valid @RequestBody PasswordChangeRequest request) {
-		repo.findById(Security.getUserId()) //
-				.filter(usr -> crypto.matches(request.getPassword(), usr.getPassword()))
-				.orElseThrow(() -> new BadRequestException("bad request"));
-		User user = repo.findById(userId).orElseThrow(() -> new ResourceNotFoundException("user", "id", userId));
-		user.setPassword(crypto.encode(request.getNewPassword()));
-		repo.save(user);
-	}
-
-	@Secured("ROLE_ADMIN")
-	@GetMapping("/user/exists")
-	public boolean existsUser(@RequestParam("username") String username) {
-		return repo.findByUsername(username).isPresent();
-	}
-
-	@Transactional
-	@Secured("ROLE_ADMIN")
-	@PostMapping("/user")
-	public void createUser(@Valid @RequestBody SignupRequest request) {
-		if (existsUser(request.getUsername()))
-			throw new BadRequestException("user '" + request.getUsername() + "' already exists");
-		User user = new User(request.getUsername(), crypto.encode(request.getPassword()), null);
-		repo.save(user);
-	}
-
-	@Secured("ROLE_ADMIN")
-	@GetMapping("/users")
-	public List<User> getUserList() {
-		val list = repo.findAll();
-		for (val user : list)
-			user.patchRoles(roleService::getCompletedRoleList);
-		return list;
-	}
-
-	@Secured("ROLE_ADMIN")
-	@GetMapping("/roles")
-	public List<Role> getRoles() {
-		return roleRepo.findAllByOrderBySortAscNameAsc();
-	}
-
-	@Secured("ROLE_ADMIN")
-	@PatchMapping("/user/{userId}/role")
-	public List<User> patchUserRole(@PathVariable("userId") Long userId, @Valid @RequestBody RoleChangeRequest request) {
-		long myId = Security.getUserId();
-		if (myId == userId)
-			throw new BadRequestException("cannot change your own roles");
-		User user = repo.findById(userId).orElseThrow(() -> new ResourceNotFoundException("user", "id", userId));
-
-		val roles = request.getRoles();
-		if (roles.contains("ROLE_ADMIN"))
-			user.setRoles(Arrays.asList("ROLE_ADMIN"));
-		else
-			user.setRoles(roles);
-		
-		repo.save(user);
-		
-		return getUserList();
-	}
-
 }
 
 @Data
@@ -148,13 +77,3 @@ class PasswordChangeRequest {
 	private @NotBlank @Size(min = 6, max = 20) String newPassword;
 }
 
-@Data
-class SignupRequest {
-	private @NotBlank @Size(min = 3, max = 15) String username;
-	private @NotBlank @Size(min = 6, max = 20) String password;
-}
-
-@Data
-class RoleChangeRequest {
-	private List<String> roles;
-}
