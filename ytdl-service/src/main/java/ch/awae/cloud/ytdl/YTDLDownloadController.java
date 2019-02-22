7610bb6596e1217f91f5a2e8af06451205b45c1a
Andreas Wälchli <andreas.waelchli@me.com>
2019-02-23 00:56:02 +0100

    fixed ytdl

--- ytdl-service/src/main/java/ch/awae/cloud/ytdl/YTDLDownloadController.java
+++ ytdl-service/src/main/java/ch/awae/cloud/ytdl/YTDLDownloadController.java
@@ -5,12 +5,11 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.google.common.net.HttpHeaders;
 
 import ch.awae.cloud.exception.ResourceNotFoundException;
 import ch.awae.cloud.ytdl.services.FileStorageService;
