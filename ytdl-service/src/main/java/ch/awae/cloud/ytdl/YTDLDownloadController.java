96e18c5bce6a21e27432b4350654d9f93fd99be4
Andreas Wälchli <andreas.waelchli@me.com>
2019-02-23 01:14:04 +0100

    attempt to fix weird file extensions using safari

--- ytdl-service/src/main/java/ch/awae/cloud/ytdl/YTDLDownloadController.java
+++ ytdl-service/src/main/java/ch/awae/cloud/ytdl/YTDLDownloadController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,6 +38,7 @@ public class YTDLDownloadController {
 			contentType = "application/octet-stream";
 
 		return ResponseEntity.ok()
+				.contentType(MediaType.parseMediaType(contentType))
 				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
 				.body(file);
 	}
